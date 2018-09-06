81cd247f7f692e42c3c67c1e21b17ecb04554b3c
anass ibnou ali <a.ibnouali@insea.ac.ma>
2018-09-06 11:51:23 +0100

    [added project] - added project to repo

---
+++ image-code/src/main/java/ma/craft/imagecode/ImageCodeApplication.java
@@ -0,0 +1,12 @@
+package ma.craft.imagecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageCodeApplication.class, args);
+	}
+}
