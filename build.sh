24249959fc1b7d5757c67dee872e4a2c6a4fc4e7
anass ibnou ali <a.ibnouali@insea.ac.ma>
2018-09-12 10:26:16 +0100

    modifs de build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,6 @@ if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [[ $TRAVIS_BRANCH = 'master'  ||  ${T
     ...
 elif [ ${TRAVIS_PULL_REQUEST} != 'false' ]; then
       echo 'Build and analyze pull request'
-      mvn -B clean verify sonar:sonar \
-      -Dsonar.host.url=${SONAR_URL} \
-      -Dsonar.projectKey=${SONAR_PROJECT} \
-      -Dsonar.organization=${SONAR_ORGANIZATION} \
-      -Dsonar.login=${SONAR_LOGIN} \
-      -Dsonar.github.oauth=${SONAR_GITHUB_OAUTH} \
-      -Dsonar.pullrequest.github.repository=${TRAVIS_REPO_SLUG}  \
-      -Dsonar.pullrequest.provider=GitHub  \
-      -Dsonar.pullrequest.branch=${TRAVIS_BRANCH}  \
-      -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST};
+      mvn -B clean verify sonar:sonar 
+      -Dsonar.host.url=${SONAR_URL} -Dsonar.projectKey=${SONAR_PROJECT} -Dsonar.organization=${SONAR_ORGANIZATION} -Dsonar.login=${SONAR_LOGIN} -Dsonar.github.oauth=${SONAR_GITHUB_OAUTH} -Dsonar.pullrequest.github.repository=${TRAVIS_REPO_SLUG} -Dsonar.pullrequest.provider=GitHub -Dsonar.pullrequest.branch=${TRAVIS_BRANCH} -Dsonar.pullrequest.key=${TRAVIS_PULL_REQUEST};
 fi
