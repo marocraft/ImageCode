197119f02b9ff8c2fd1d7c068b92dfb68b7019ac
anass ibnou ali <a.ibnouali@insea.ac.ma>
2019-01-29 23:10:21 +0100

    [refactorin] - fixing quality gate blockers issues

--- src/main/java/ma/craft/imagecode/parsing/Parser.java
+++ src/main/java/ma/craft/imagecode/parsing/Parser.java
@@ -1,7 +1,5 @@
 package ma.craft.imagecode.parsing;
 
-import java.io.FileNotFoundException;
-
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
@@ -9,18 +7,18 @@ import org.json.JSONTokener;
 
 public class Parser {
 
-	public JSONObject readJsonFile(String path) throws FileNotFoundException {
+	public JSONObject readJsonFile(String path) {
 		ClassLoader classLoader = Parser.class.getClassLoader();
 		return new JSONObject(new JSONTokener(classLoader.getResourceAsStream(path)));
 
 	}
 
-	public Schema loadSchema(String path) throws FileNotFoundException {
+	public Schema loadSchema(String path) {
 		return SchemaLoader.load(this.readJsonFile(path));
 
 	}
 
-	public void validateSchema(String dataFile, String schemaFile) throws FileNotFoundException {
+	public void validateSchema(String dataFile, String schemaFile) {
 		Schema schema = SchemaLoader.load(this.readJsonFile(schemaFile));
 		schema.validate(this.readJsonFile(dataFile));
 	}
