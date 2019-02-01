076edc5428efe38d3f4f603aee2ce250db984b4f
anass ibnou ali <a.ibnouali@insea.ac.ma>
2019-02-02 00:05:54 +0100

    [refactoring] - fixing unit test

--- src/main/java/ma/craft/imagecode/domain/Child.java
+++ src/main/java/ma/craft/imagecode/domain/Child.java
@@ -2,9 +2,11 @@ package ma.craft.imagecode.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Child {
 
 	private String name;
